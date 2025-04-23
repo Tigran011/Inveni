5e75238a3ee2dbc03a95e9654bb27ffdd85b8f39
Tigran011 <119415686+Tigran011@users.noreply.github.com>
2025-04-23 16:44:25 +0400

    inveni v1

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+# inveni/core/__init__.py
+from core.version_manager import VersionManager
+from core.backup_manager import BackupManager
+from core.settings import SettingsManager
+from core.file_monitor import FileMonitor
